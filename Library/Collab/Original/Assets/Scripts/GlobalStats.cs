c9931bb5a733a2379ebdb0c293b2c930e87cb0d7
Matthew <mattphan8264@gmail.com>
2019-05-09 13:58:34 -0700

    Main Upload

---
+++ Library/Collab/Original/Assets/Scripts/GlobalStats.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalStats : MonoBehaviour {
+
+	/*
+	Class that holds global variables for gameplay
+	Used to reduce calculation costs and make
+	easy access for stats
+	 */
+	public static int bulletDamage;
+	public static float bulletSpeed;
+	public static float attackRate;
+	public static int maxHealth;
+	public static float rushEnemySpeed;
+	public static int rushEnemyDamage;
+	public static int enemyHealth;
+	public static float enemySpawnRate;
+	public static float enemySpawnX;
+	public static float enemySpawnY;
+	public static float enemySpawnZ;
+	public static float shieldEnemyBlockRate;
+
+	
+
+	void Awake()
+	{
+		bulletDamage = 1;
+		bulletSpeed = .1f;
+		attackRate = 1f;
+		maxHealth = 100;
+
+		rushEnemySpeed = .1f;
+		rushEnemyDamage = 1;
+		enemyHealth = 1;
+		enemySpawnRate = 3f;
+
+		enemySpawnX = .5f;
+		enemySpawnY = .5f;
+		enemySpawnZ = .5f;
+
+		shieldEnemyBlockRate = 1f;
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
